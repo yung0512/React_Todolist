20c2b261ac3231bbd70677f6ee66fa8dff16ab5e
yung0512 <jeff8651215@gmail.com>
2021-04-12 10:48:06 +0800

    add todo form input hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,10 +1,21 @@
-import React  from 'react'
+import React ,{useState}from 'react'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import AppBar from '@material-ui/core/AppBar'
+import TodoList from './TodoList'
+import TodoForm from './TodoForm'
 import Toolbar from '@material-ui/core/Toolbar'
 import Grid from '@material-ui/core/Grid'
 function TodoApp(){
+    const initialTodos = [
+        {id:1,task:"Clean Fishtank",completed:false},
+        {id:2,task:"jogging 3000m",completed:false},
+        {id:3,task:"feed cats",completed:false}
+    ]
+    const [todos, setTodos] = useState(initialTodos)
+    const addTodo = newTodoText => {
+        setTodos([...todos,{id:4,task:newTodoText,completed:false}])
+    }
     return (
         <Paper
             style={{
@@ -22,6 +33,8 @@ function TodoApp(){
                     </Typography>
                 </Toolbar>
             </AppBar>
+            <TodoForm addTodo={addTodo}/>
+                <TodoList todos={todos}/>
         </Paper>
     )
 }
